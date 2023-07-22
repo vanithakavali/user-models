28a8016747759ec3a3b0f40a8258ef854ce7ace1
vanithakavali <kavalivanitheswari@gmail.com>
2023-07-22 14:21:48 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 from django.core.mail import send_mail
 from app.forms import *
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+
 def registration(request):
     usfo=UserForm()
     pfo=ProfileForm()
@@ -24,7 +28,7 @@ def registration(request):
 
             send_mail('Registration',
                     'Ur Registration is Successfull',
-                      'shaikharshadvali79@gmail.com',
+                      'kavalivanitheswari@gmail.com',
                       [NSUFO.email],
                       fail_silently=False
                           )
@@ -36,3 +40,37 @@ def registration(request):
 
 
     return render(request,'registration.html',d)
+
+
+def home(request):
+
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
+
+    return render(request,'home.html')
+
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        AUO=authenticate(username=username,password=password)
+        if AUO:
+            if AUO.is_active:
+                login(request,AUO)
+                request.session['username']=username
+                return HttpResponseRedirect(reverse('home'))
+            else:
+                return HttpResponse('Not a Active User')
+        else:
+            return HttpResponse('Invalid Details')
+    return render(request,'user_login.html')
+
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
