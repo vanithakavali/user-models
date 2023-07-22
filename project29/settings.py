4f8faa227a1e2707279fa832710681c323ce39dc
vanithakavali <kavalivanitheswari@gmail.com>
2023-07-22 13:44:28 +0530

    first commit

--- project29/settings.py
+++ project29/settings.py
@@ -126,3 +126,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL='media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER='kavalivanitheswari@gmail.com'
+EMAIL_HOST_PASSWORD='xakilhqpywwoijzi'
+EMAIL_USE_TLS=True
+EMAIL_USE_SSL=False
+EMAIL_PORT=587
--- project29/urls.py
+++ project29/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for project29 project.
+URL configuration for project23 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -16,13 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-
 from django.conf import settings
 from django.conf.urls.static import static
-
+from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('registration/',registration,name='registration'),
 
 
 
